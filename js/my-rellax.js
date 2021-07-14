310c90658720a709f5bee03d84f0baf2ffe91354
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-07-14 10:11:44 +0900

    Add files via upload

---
+++ js/my-rellax.js
@@ -0,0 +1,31 @@
+var rellax = new Rellax('.rellax-slow01', {
+    speed: -2,
+  });
+
+var rellax = new Rellax('.rellax-slow02', {
+    speed: -5,
+  });
+
+var rellax = new Rellax('.rellax-slow03', {
+    speed: -7,
+  });
+
+var rellax = new Rellax('.rellax-slow04', {
+    speed: -10,
+  });
+
+var rellax = new Rellax('.rellax-fast01', {
+    speed: 2,
+  });
+
+var rellax = new Rellax('.rellax-fast02', {
+    speed: 5,
+  });
+
+var rellax = new Rellax('.rellax-fast03', {
+    speed: 7,
+  });
+
+var rellax = new Rellax('.rellax-fast04', {
+    speed: 10,
+  });
