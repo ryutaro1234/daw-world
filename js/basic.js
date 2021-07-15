ac86a652da52b3e58c67f6cc48f63fc188fc883d
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-07-15 20:48:17 +0900

    Add files via upload

--- js/basic.js
+++ js/basic.js
@@ -19,3 +19,25 @@ audio01.onpause=function(){
   document.getElementById("basic-b01").classList.remove("show07")
   audio01.currentTime = 0;
 };
+
+var audio02 = document.getElementById("audio02");
+audio02.onplay=function(){
+  document.getElementById("basic-c02").classList.add("show01")
+  document.getElementById("basic-d02").classList.add("show02")
+  document.getElementById("basic-e02").classList.add("show03")
+  document.getElementById("basic-f02").classList.add("show04")
+  document.getElementById("basic-g02").classList.add("show05")
+  document.getElementById("basic-a02").classList.add("show06")
+  document.getElementById("basic-b02").classList.add("show07")
+};
+
+audio02.onpause=function(){
+  document.getElementById("basic-c02").classList.remove("show01")
+  document.getElementById("basic-d02").classList.remove("show02")
+  document.getElementById("basic-e02").classList.remove("show03")
+  document.getElementById("basic-f02").classList.remove("show04")
+  document.getElementById("basic-g02").classList.remove("show05")
+  document.getElementById("basic-a02").classList.remove("show06")
+  document.getElementById("basic-b02").classList.remove("show07")
+  audio02.currentTime = 0;
+};
