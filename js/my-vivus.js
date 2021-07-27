f385fcb07761111bb839b4a127d0176c2bd76824
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-07-27 19:36:41 +0900

    2021-0727

--- js/my-vivus.js
+++ js/my-vivus.js
@@ -13,3 +13,11 @@ new Vivus('svg02', {
     pathTimingFunction: Vivus.EASE_IN,
     forceRender: false,
 });
+
+new Vivus('svg03', {
+    type: 'delayed',
+    duration: 100,
+    start: 'inViewport',
+    pathTimingFunction: Vivus.EASE_IN,
+    forceRender: false,
+});
