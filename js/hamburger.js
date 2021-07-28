090fb279cfb7deb79d65bbd42b163082f5b878fd
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-07-28 10:13:36 +0900

    2021-0728

--- js/hamburger.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-$('.nav_toggle').on('click', function () {
-    $('.nav_toggle, .nav, #nav').toggleClass('show');
-});
-})
--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 $(function(){
-  $(window).on('load scroll',function (){
-    $('.scroll-up').each(function(){
-      var target = $(this).offset().top;
-      var scroll = $(window).scrollTop();
-      var height = $(window).height();
-      if (scroll > target - height){
-        $(this).addClass('active');
-      }
-    });
-  });
+$('.nav_toggle').on('click', function () {
+		$('.nav_toggle, .nav, #nav').toggleClass('show');
+});
+})
+
+
+$(function(){
+	$(window).on('load scroll',function (){
+		$('.scroll-up').each(function(){
+			var target = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var height = $(window).height();
+			if (scroll > target - height){
+				$(this).addClass('active');
+			}
+		});
+	});
 });
