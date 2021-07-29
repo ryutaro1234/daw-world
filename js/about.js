58017f5e324a17c0ff1a05301c15bd4176c2f2af
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-07-29 15:31:22 +0900

    2021-0729

--- js/about.js
+++ js/about.js
@@ -20,6 +20,7 @@ audio01.onplay=function(){
 };
 
 audio01.onpause=function(){
+	audio01.currentTime = 0;
 	document.getElementById("basic-c01").classList.remove("show01")
 	document.getElementById("basic-d01").classList.remove("show02")
 	document.getElementById("basic-e01").classList.remove("show03")
@@ -37,5 +38,4 @@ audio01.onpause=function(){
 	document.getElementById("table01-a01").classList.remove("show06")
 	document.getElementById("table01-b01").classList.remove("show07")
 	document.getElementById("table01-c02").classList.remove("show08")
-	audio01.currentTime = 0;
 };
