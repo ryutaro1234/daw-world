94d47acfba82e359bcc24bc3891841e1542ca75a
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-08-04 09:34:08 +0900

    2021-0804

---
+++ js/toppage.js
@@ -0,0 +1,27 @@
+var rellax = new Rellax('#shape01', {
+		speed: 3,
+	});
+
+var rellax = new Rellax('#shape02', {
+		speed: -2,
+	});
+
+var rellax = new Rellax('#shape03', {
+		speed: -1,
+	});
+
+var rellax = new Rellax('#shape04', {
+		speed: 2,
+	});
+
+var rellax = new Rellax('#shape05', {
+		speed: -2,
+	});
+
+var rellax = new Rellax('#shape06', {
+		speed: 3,
+	});
+
+var rellax = new Rellax('#shape07', {
+		speed: -1,
+	});
