4162c70089d057c7f115f76472b099f53480de0b
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-08-10 17:53:12 +0900

    2021-0810

--- js/toppage.js
+++ js/toppage.js
@@ -5,40 +5,61 @@ window.addEventListener("load", () => {
 window.addEventListener("load", () => {
   document.getElementById("bg01").classList.add("-visible");
 });
-var rellax = new Rellax('#bg01', {
-  speed: -6,
-});
 
 window.addEventListener("load", () => {
   document.getElementById("bg02").classList.add("-visible");
-});
-var rellax = new Rellax('#bg02', {
-  speed: -3,
 });
 
 window.addEventListener("load", () => {
   document.getElementById("title01").classList.add("-visible");
 });
 
-let mainimage = document.getElementById("main-image");
 window.addEventListener("load", () => {
-  mainimage.classList.add("active");
+  document.getElementById("main-image").classList.add("active");
 });
 
 window.addEventListener("load", () => {
   document.getElementById("bg03").classList.add("-visible");
 });
-var rellax = new Rellax('#bg03', {
-  speed: 5,
-});
 
 window.addEventListener("load", () => {
   document.getElementById("bg04").classList.add("-visible");
 });
-var rellax = new Rellax('#bg04', {
-  speed: 2,
-});
 
 window.addEventListener("load", () => {
   document.getElementById("type02").classList.add("active");
+});
+
+
+$(function(){
+
+  var bg01 = $('#bg01').offset(),
+      bg02 = $('#bg02').offset(),
+      bg03 = $('#bg03').offset(),
+      bg04 = $('#bg04').offset();
+
+  $(window).on('scroll',function(){
+    var scrollY = $(this).scrollTop();
+    console.log(scrollY);
+
+    $('#bg01').css({
+      'top': + scrollY / 8,
+      'left': bg01.left + scrollY / 10
+    });
+
+    $('#bg02').css({
+      'top': 50 + scrollY / 2,
+      'left': bg02.left + scrollY / 8
+    });
+
+    $('#bg03').css({
+      'top': bg03.bottom + scrollY / 5,
+      'left': bg03.left + scrollY / 2
+    });
+
+    $('#bg04').css({
+      'top': bg04.bottom + scrollY / 1,
+      'left': bg04.left + scrollY / -5
+    });
+  });
 });
