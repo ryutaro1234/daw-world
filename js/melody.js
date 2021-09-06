d8ce8a2c19874d0c8acd9a3a85f8921fb5983e9f
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-09-07 07:51:30 +0900

    update melody

--- js/melody.js
+++ js/melody.js
@@ -19,3 +19,39 @@ audio01.onpause = function(){
   document.querySelector("#g").classList.remove("play01");
   document.querySelector("#c").classList.remove("play01");
 }
+
+function melody01buttom01(){
+  document.querySelector("#melody01buttom01").classList.add("click");
+  document.querySelector("#audio02").play();
+}
+function melody01buttom02(){
+  document.querySelector("#audio02").pause();
+}
+
+var audio02 = document.querySelector("#audio02");
+audio02.onplay = function(){
+  document.querySelector("#melody01buttom02").classList.add("click");
+}
+audio02.onpause = function(){
+  audio02.currentTime = 0;
+  document.querySelector("#melody01buttom02").classList.remove("click");
+  document.querySelector("#melody01buttom01").classList.remove("click");
+}
+
+function melody02buttom01(){
+  document.querySelector("#melody02buttom01").classList.add("click");
+  document.querySelector("#audio03").play();
+}
+function melody02buttom02(){
+  document.querySelector("#audio03").pause();
+}
+
+var audio03 = document.querySelector("#audio03");
+audio03.onplay = function(){
+  document.querySelector("#melody02buttom02").classList.add("click");
+}
+audio03.onpause = function(){
+  audio03.currentTime = 0;
+  document.querySelector("#melody02buttom02").classList.remove("click");
+  document.querySelector("#melody02buttom01").classList.remove("click");
+}
